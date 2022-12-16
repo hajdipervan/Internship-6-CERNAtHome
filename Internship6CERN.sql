b8864a292caf1824778b6f18a77f6d2d2dc34b68
Hajdi Pervan <hajdi.pervan5@gmail.com>
2022-12-16 13:46:32 +0100

    Added print 3, 4, 5, 6, 7

--- Internship6CERN.sql
+++ Internship6CERN.sql
@@ -12,18 +12,18 @@ CREATE TABLE Accelerators(
 	Id SERIAL PRIMARY KEY,
 	Name VARCHAR(50) NOT NULL
 )
+-------------------
+CREATE TABLE Projects(
+	Id SERIAL PRIMARY KEY,
+	Name VARCHAR(50) NOT NULL
+)
 ------------------
 CREATE TABLE ScientificWorks(
 	Id SERIAL PRIMARY KEY,
 	Name VARCHAR(50) NOT NULL,
 	NumberOfQuotes INT,
-	ReleaseDate TIMESTAMP NOT NULL
-)
--------------------
-CREATE TABLE Projects(
-	Id SERIAL PRIMARY KEY,
-	Name VARCHAR(50) NOT NULL,
-	ScientificWorkId INT REFERENCES ScientificWorks(Id)
+	ReleaseDate TIMESTAMP NOT NULL,
+	UsedInProjectId INT REFERENCES Projects(Id)
 )
 -------------------
 CREATE TABLE AcceleratorProject(
@@ -63,7 +63,7 @@ CREATE TABLE ScientistScientificWork(
 -------------------
 -------------------
 -------PRINT-------
---1--
+--1-- RADI
 --naziv i datum objave svakog znanstvenog rada zajedno s imenima znanstvenika koji su na njemu radili, 
 --pri čemu imena znanstvenika moraju biti u jednoj ćeliji i u obliku Prezime, I.; npr. Puljak, I.; Godinović, N.; Bilušić, A.
 SELECT sw.Name, sw.ReleaseDate, STRING_AGG(CONCAT(s.Surname, ', ', LEFT(s.Name, 1)), '; ') FROM ScientificWorks sw 
@@ -71,7 +71,7 @@ JOIN ScientistScientificWork ssw ON ssw.ScientificWorkId=sw.Id
 JOIN Scientists s ON s.Id=ssw.ScientistId
 GROUP BY sw.Name, sw.ReleaseDate
 
---2--
+--2-- RADI
 --ime, prezime, spol (ispisati ‘MUŠKI’, ‘ŽENSKI’, ‘NEPOZNATO’, ‘OSTALO’;), ime države i  PPP/capita iste svakom znanstveniku--
 SELECT s.Name, s.Surname,
 CASE WHEN s.Gender='0' THEN 'Not known'
@@ -84,8 +84,64 @@ FROM Scientists s
 JOIN Countries c ON c.Id=s.CountryId
 --DODAJ OVAJ PPP?
 
---3--
---
+--3-- NE RADI
+--svaku kombinaciju projekta i akceleratora, pri čemu nas zanimaju samo nazivi; 
+--u slučaju da projekt nije vezan ni za jedan akcelerator, svejedno ga ispiši uz ime akceleratora ‘NEMA GA’.
+
+SELECT ms.Name,
+	COALESCE(CAST((SELECT MAX(st.CreatedAt) FROM Streams st
+	 	JOIN Songs sg ON sg.Id=st.SongId
+	 	JOIN SongMusicians sm ON sg.Id=sm.SongId
+	 	JOIN Musicians m ON m.Id=sm.MusicianId
+	 WHERE ms.Id=m.Id) 	AS VARCHAR), 'None') AS LastStream
+FROM Musicians ms
+
+--4-- RADI
+--sve projekte kojima je bar jedan od radova izašao između 2015. i 2017.
+SELECT p.Name FROM Projects p 
+JOIN ScientificWorks sw ON sw.UsedInProjectId=p.Id
+WHERE DATE_PART('year', sw.ReleaseDate)>2014 AND DATE_PART('year', sw.ReleaseDate)<2017
+
+--5-- RADI
+--u istoj tablici po zemlji broj radova i najpopularniji rad znanstvenika iste zemlje, 
+--pri čemu je najpopularniji rad onaj koji ima najviše citata
+SELECT c.Name, COUNT(sw.Id),
+CAST((SELECT swn.Name FROM ScientificWorks swn
+	JOIN ScientistScientificWork sswn ON sswn.ScientificWorkId=swn.Id
+	JOIN Scientists sn ON sn.Id=sswn.ScientistId
+	JOIN Countries cn ON cn.Id=sn.CountryId
+	WHERE c.Name=cn.Name
+	ORDER BY swn.NumberOfQuotes DESC
+	LIMIT 1) AS VARCHAR)
+FROM Countries c
+JOIN Scientists s ON s.CountryId=c.Id
+JOIN ScientistScientificWork ssw ON ssw.ScientistId=s.Id
+JOIN ScientificWorks sw ON sw.Id=ssw.ScientificWorkId
+GROUP BY c.Name
+
+--6--prvi objavljeni rad po svakoj zemlji
+SELECT DISTINCT ON(c.Name) c.Name, sw.Name, sw.ReleaseDate FROM Countries c
+JOIN Scientists s ON s.CountryId=c.Id
+JOIN ScientistScientificWork ssw ON ssw.ScientistId=s.Id
+JOIN ScientificWorks sw ON sw.Id=ssw.ScientificWorkId
+ORDER BY c.Name, sw.ReleaseDate
+
+--7--gradove po broju znanstvenika koji trenutno u njemu borave
+SELECT h.City, COUNT(s.Id) AS NumberOfScientists FROM Hotels h
+FULL JOIN Scientists s ON s.HotelId=h.Id
+GROUP BY h.City
+ORDER BY COUNT(s.Id) DESC
+
+--8--prosječan broj citata radova po svakom akceleratoru
+
+
+--ok kod--
+SELECT c.Name, COUNT(sw.Id) FROM Countries c
+JOIN Scientists s ON s.CountryId=c.Id
+JOIN ScientistScientificWork ssw ON ssw.ScientistId=s.Id
+JOIN ScientificWorks sw ON sw.Id=ssw.ScientificWorkId
+GROUP BY c.Name
+
 SELECT * FROM Hotels
 SELECT * FROM Countries
 SELECT * FROM Scientists
@@ -107,10 +163,17 @@ INSERT INTO Scientists(Id, Name, Surname, BirthDate, Gender, Profession, Country
 (default, 'Marija', 'Bliznac Trebjesanin', '1975-11-30', '2', 'engineer', 2, 3),
 (default, 'Darko', 'Vujica', '2000-10-10', '1', 'developer', 3, 1)
 
-INSERT INTO ScientificWorks(Id, Name, NumberOfQuotes, ReleaseDate) VALUES
-(default, 'Logaritamska funkcija', 30, '2010-5-6'),
-(default, 'Funkcije', 20, '2011-5-10'),
-(default, 'Bozja cestica', 25, '2015-10-6')
+INSERT INTO Projects(Id, Name) VALUES
+(default, 'projekt1'),
+(default, 'projekt2'),
+(default, 'projekt3'),
+(default, 'projekt4'),
+(default, 'projekt5')
+
+INSERT INTO ScientificWorks(Id, Name, NumberOfQuotes, ReleaseDate, UsedInProjectId) VALUES
+(default, 'Logaritamska funkcija', 30, '2010-5-6', 1),
+(default, 'Funkcije', 20, '2011-5-10', 1),
+(default, 'Bozja cestica', 25, '2015-10-6', 2)
 
 INSERT INTO ScientistScientificWork(ScientistId,ScientificWorkId ) VALUES
 (1, 1),
@@ -120,3 +183,23 @@ INSERT INTO ScientistScientificWork(ScientistId,ScientificWorkId ) VALUES
 (1, 3),
 (2, 3),
 (3, 3)
+
+INSERT INTO Accelerators(Id, Name) VALUES
+(default, 'accelerator1'),
+(default, 'accelerator2'),
+(default, 'accelerator3'),
+(default, 'accelerator4'),
+(default, 'accelerator5'),
+(default, 'accelerator6')
+
+INSERT INTO AcceleratorProject(AcceleratorId, ProjectId) VALUES
+(1, 1),
+(1, 2),
+(2, 1),
+(3, 2),
+(4, 1),
+(4, 2),
+(4, 3),
+(5, 1),
+(5, 2),
+(6, 1)
